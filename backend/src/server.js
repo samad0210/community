791765a76e51ab21fed51f65922a6fcfff8a922e
samad0210 <khansamadabdul0202@gmail.com>
2026-02-13 00:50:44 +0530

    fix cors url

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['https://community-82c5k7r9f-abduls-projects-b66bcbf7.vercel.app/', 'http://localhost:5173'],
+  origin: ['https://community-app-teal.vercel.app/', 'http://localhost:5173'],
   credentials: true
 }));
 
